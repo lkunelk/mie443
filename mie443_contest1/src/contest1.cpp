cf31abd7d6f4de69890fd461a0a54415a6d08a14
Yuxiang Dai <yuxiang.dai43@gmail.com>
2020-01-21 17:26:58 -0500

    Tutorial 2 Template Code

--- mie443_contest1/src/contest1.cpp
+++ mie443_contest1/src/contest1.cpp
@@ -3,14 +3,20 @@
 #include <geometry_msgs/Twist.h>
 #include <kobuki_msgs/BumperEvent.h>
 #include <sensor_msgs/LaserScan.h>
-
+#include<nav_msgs/Odometry.h>
+#include<tf/transform_datatypes.h>
 #include <stdio.h>
 #include <cmath>
 
 #include <chrono>
 
-#include<nav_msgs/Odometry.h>
-#include<tf/transform_datatypes.h>
+#define N_BUMPER (3)
+#define RAD2DEG(rad)((rad)*180./M_PI)
+#define DEG2RAD(deg)((deg)*M_PI/180.)
+
+uint8_t bumper[3] = {kobuki_msgs::BumperEvent::RELEASED, kobuki_msgs::BumperEvent::RELEASED, kobuki_msgs::BumperEvent::RELEASED};
+float minLaserDist = std::numeric_limits<float>::infinity();
+int32_t nLasers = 0, desiredNLasers = 0, desiredAngle = 5;
 
 float angular = 0.0;
 float linear = 0.0;
@@ -18,17 +24,40 @@ float posX = 0.0, posY = 0.0, yaw = 0.0;
 
 void odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
 {
-    //do something
+    posX = msg->pose.pose.position.x;
+    posY = msg->pose.pose.position.y;
+    yaw = tf::getYaw(msg->pose.pose.orientation);
+    ROS_INFO("Position: (%f,%f) Orientation: %f rad or %f degrees.", posX, posY, yaw, RAD2DEG(yaw));
 }
 
-void bumperCallback(const kobuki_msgs::BumperEvent::ConstPtr& msg)
+void bumperCallback(const kobuki_msgs::BumperEvent::ConstPtr &msg)
 {
-	//fill with your code
+    // 
+    // Access using bumper[kobuki_msgs::BumperEvent::{}] LEFT, CENTER, or RIGHT
+    bumper[msg->bumper] = msg->state;
 }
 
 void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
-	//fill with your code
+    minLaserDist = std::numeric_limits<float>::infinity();
+    nLasers = (msg->angle_max - msg->angle_min) / msg->angle_increment;
+    desiredNLasers = DEG2RAD(desiredAngle) / msg->angle_increment;
+    ROS_INFO("Size of laser scan array: % i and size of offset: % i ", nLasers, desiredNLasers);
+
+    if (desiredAngle * M_PI / 180 < msg -> angle_max && -desiredAngle * M_PI / 180 > msg -> angle_min)
+    {
+        for (uint32_t laser_idx = nLasers / 2 - desiredNLasers; laser_idx < nLasers / 2 + desiredNLasers; ++laser_idx)
+        {
+            minLaserDist = std::min(minLaserDist, msg -> ranges[laser_idx]);
+        }
+    }
+    else
+    {
+        for (uint32_t laser_idx = 0; laser_idx < nLasers; ++laser_idx)
+        {
+            minLaserDist = std::min(minLaserDist, msg -> ranges[laser_idx]);
+        }
+    }
 }
 
 int main(int argc, char **argv)
@@ -53,7 +82,35 @@ int main(int argc, char **argv)
 
     while(ros::ok() && secondsElapsed <= 480) {
         ros::spinOnce();
-        //fill with your code
+
+        ROS_INFO("Position: (%f,%f) Orientation: %f degrees Range: %f", posX, posY, RAD2DEG(yaw), minLaserDist);
+
+        /*    
+        //
+        // Check if any of the bumpers were pressed.
+        bool any_bumper_pressed = false;
+        for (uint32_t b_idx = 0; b_idx < N_BUMPER; ++b_idx)
+        {
+            any_bumper_pressed |= (bumper[b_idx] == kobuki_msgs ::BumperEvent ::PRESSED);
+        } 
+        // 
+        // Control logic after bumpers are being pressed.
+        if (posX < 0.5 && yaw < M_PI / 12 && !any_bumper_pressed)
+        {
+            angular = 0.0;
+            linear = 0.2;
+        }
+        else if (yaw < M_PI / 2 && posX > 0.5 && !any_bumper_pressed)
+        {
+            angular = M_PI / 6;
+            linear = 0.0;
+        }
+        else
+        {
+            angular = 0.0;
+            linear = 0.0;
+        }
+        */
 
         vel.angular.z = angular;
         vel.linear.x = linear;
