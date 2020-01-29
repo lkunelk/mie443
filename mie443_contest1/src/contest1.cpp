e7095dd47666805455b0dee4af6f11d8601f4e87
NelsonLee-NTU <a06w.68577818@gmail.com>
2020-01-28 19:41:17 -0500

    Drive function

--- mie443_contest1/src/contest1.cpp
+++ mie443_contest1/src/contest1.cpp
@@ -60,6 +60,27 @@ void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
     }
 }
 
+// The code to drive to the desired point
+void drive(float xd, float yd) {
+    odomCallback();
+    float distance = ((posX-xd)^2+(posY-yd)^2)^0.5;
+    float angle = atan((yd-posY)/(xd-posX))*180/M_PI;
+    float rotation;
+    if(angle - yaw > 180) {
+        rotation = -(360-(angle - yaw));
+    }
+    else if(angle - yaw < -180) {
+        rotation = -360 - (angle - yaw);
+    }
+    else {
+        rotation = angle - yaw;
+    }
+
+    turn(rotation);
+    forward(distance);
+
+}
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "image_listener");
@@ -119,6 +140,9 @@ int main(int argc, char **argv)
         // The last thing to do is to update the timer.
         secondsElapsed = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now()-start).count();
         loop_rate.sleep();
+
+
+        
     }
 
     return 0;
