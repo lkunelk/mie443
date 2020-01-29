9ccf1e446fa3bb8321f61f3f8022cd763170db4c
Nam <lkunelk@gmail.com>
2020-01-28 19:53:20 -0500

    succesfully subscribed to map topic

--- mie443_contest1/src/gmap_to_index.cpp
+++ mie443_contest1/src/gmap_to_index.cpp
@@ -1,4 +1,24 @@
 //
-// Created by nam on 2020-01-28.
+// read gmap map and transforms
+// publish map and index
 //
 
+#include <ros/console.h>
+#include "ros/ros.h"
+#include "nav_msgs/OccupancyGrid.h"
+
+void mapCallback(const nav_msgs::OccupancyGrid::ConstPtr& msg)
+{
+    ROS_INFO_STREAM("map!");
+}
+
+int main(int argc, char **argv)
+{
+    ROS_INFO_STREAM("Starting!");
+    ros::init(argc, argv, "nam_node");
+    ros::NodeHandle nh;
+    ros::Subscriber map_sub = nh.subscribe("/map", 10, &mapCallback);
+
+    ros::spin();
+    return 0;
+}
