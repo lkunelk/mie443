d97baad47cc820e3ddd6b98ba67907aa0d9cda0f
Yuxiang Dai <yuxiang.dai43@gmail.com>
2020-01-28 19:23:00 -0500

    2D Array and Position Array example functions

--- mie443_contest1/src/contest1.cpp
+++ mie443_contest1/src/contest1.cpp
@@ -14,11 +14,6 @@
 #define RAD2DEG(rad)((rad)*180./M_PI)
 #define DEG2RAD(deg)((deg)*M_PI/180.)
 
-
-
-
-
-
 uint8_t bumper[3] = {kobuki_msgs::BumperEvent::RELEASED, kobuki_msgs::BumperEvent::RELEASED, kobuki_msgs::BumperEvent::RELEASED};
 float minLaserDist = std::numeric_limits<float>::infinity();
 int32_t nLasers = 0, desiredNLasers = 0, desiredAngle = 5;
@@ -45,15 +40,15 @@ void bumperCallback(const kobuki_msgs::BumperEvent::ConstPtr &msg)
 class Position 
 {
     uint64_t secondsElapsed;
-    float x, y;
+    int x, y;
 	public:
-        void setPosition(float posX, float posY)
+        void setPosition(int posX, int posY)
         {
             x = posX;
             y = posY;
         }
-        float getX() { return x; }
-        float getY() { return y; }
+        int getX() { return x; }
+        int getY() { return y; }
 
 };
 
@@ -77,17 +72,58 @@ class Memory
         }
 };
 
+// Returns a pointer to a newly created 2d array the array2D has size [height x width]
 
-// void testArray() {
-// 	Student st[5];
-// 	for( int i=0; i<5; i++ )
-// 	{
-// 		st[i].setPos(i, i);
-// 		cout << "Enter marks" << endl;
-// 		st[i].getMarks();
-// 	}
-// }
+int** create2DArray(unsigned height, unsigned width)
+{
+    int** array2D = 0;
+    array2D = new int*[height];
 
+    for (int h = 0; h < height; h++)
+    {
+        array2D[h] = new int[width];
+
+        for (int w = 0; w < width; w++)
+        {
+                // fill in some initial values
+                // (filling in zeros would be more logic, but this is just for the example)
+                array2D[h][w] = w + width * h;
+        }
+    }
+
+    return array2D;
+}
+
+
+void output2DArray(int** my2DArray, int height, int width) 
+{ 
+    for (int h = 0; h < height; h++)
+    {
+        for (int w = 0; w < width; w++)
+        {
+                printf("%i,", my2DArray[h][w]);
+        }
+        printf("\n");
+    }
+}
+
+// Template function to output Position array
+Position* input2DArrayOutputPositionArray(int** my2DArray, int height, int width){
+    Position* array2D = 0;
+    array2D = new Position[height * width];
+
+    for (int h = 0; h < height; h++)
+    {
+        for (int w = 0; w < width; w++)
+        {
+            array2D[w + width * h].setPosition(h, w);
+            // printf("%i,", w + width * h);
+        }
+    }
+
+    return array2D;
+
+}
 
 
 void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
@@ -115,16 +151,16 @@ void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "image_listener");
-    ros::NodeHandle nh;
+    // ros::init(argc, argv, "image_listener");
+    // ros::NodeHandle nh;
 
-    ros::Subscriber bumper_sub = nh.subscribe("mobile_base/events/bumper", 10, &bumperCallback);
-    ros::Subscriber laser_sub = nh.subscribe("scan", 10, &laserCallback);
+    // ros::Subscriber bumper_sub = nh.subscribe("mobile_base/events/bumper", 10, &bumperCallback);
+    // ros::Subscriber laser_sub = nh.subscribe("scan", 10, &laserCallback);
 
-    ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop", 1);
-    ros::Subscriber odom = nh.subscribe("odom", 1, &odomCallback);
+    // ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop", 1);
+    // ros::Subscriber odom = nh.subscribe("odom", 1, &odomCallback);
 
-    ros::Rate loop_rate(10);
+    // ros::Rate loop_rate(10);
 
     geometry_msgs::Twist vel;
 
@@ -133,19 +169,55 @@ int main(int argc, char **argv)
     start = std::chrono::system_clock::now();
     uint64_t secondsElapsed = 0;
 
-	Memory mem;
-    float number = 1.1234;
-	for( int i=0; i<5; i++ )
-	{ 
-        number = number + 1.000;
-		mem.setPosition(number, number * 2);
-	}
+    // // Testing Memory and Position class
+	// Memory mem;
+    // float number = 1.1234;
+	// for( int i=0; i<5; i++ )
+	// { 
+    //     number = number + 1.000;
+	// 	mem.setPosition(number, number * 2);
+	// }
 
-	for( int i=0; i<5; i++ )
-	{ 
-        mem.getPosition(i);
-	}
+	// for( int i=0; i<5; i++ )
+	// { 
+    //     mem.getPosition(i);
+	// }
 
+    printf("Creating a 2D array2D\n");
+    printf("\n");
+
+    int height = 5;
+    int width = 10;
+    int** my2DArray = create2DArray(height, width);
+
+    // int** arrayOfCoords = DFS(my2DArray)
+    
+    printf("Array sized [%i,%i] created.\n\n", height, width);
+
+    // print contents of the array2D
+    printf("Array contents: \n");
+
+    // Function to print 2D array
+    output2DArray(my2DArray, height, width);
+
+    Position* output = testFunction(height, width);
+    for (int w = 0; w < height * width; w++)
+    {
+        printf("%i, %i", output[w].getX(), output[w].getY());
+        printf("\n");
+    }
+    printf("\n");
+
+    // important: clean up memory
+    printf("\n");
+    printf("Cleaning up memory...\n");
+    for ( int h = 0; h < height; h++)
+    {
+        delete [] my2DArray[h];
+    }
+    delete [] my2DArray;
+    my2DArray = 0;
+    printf("Ready.\n");
 
 
 
