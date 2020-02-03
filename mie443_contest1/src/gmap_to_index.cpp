abafb79b553bf87ac04361476964db5deb991de1
Nam <lkunelk@gmail.com>
2020-02-02 21:43:31 -0500

    obtained transform from map to robot pose

--- mie443_contest1/src/gmap_to_index.cpp
+++ mie443_contest1/src/gmap_to_index.cpp
@@ -6,10 +6,13 @@
 #include <ros/console.h>
 #include "ros/ros.h"
 #include "nav_msgs/OccupancyGrid.h"
+#include <tf/transform_listener.h>
+#include <geometry_msgs/Point.h>
 
 void mapCallback(const nav_msgs::OccupancyGrid::ConstPtr& msg)
 {
-    ROS_INFO_STREAM("map!");
+    geometry_msgs::Point p = msg->info.origin.position;
+    ROS_INFO_STREAM("map! " << p.x << ", " << p.y << ", " << p.z << "\n");
 }
 
 int main(int argc, char **argv)
@@ -19,6 +22,18 @@ int main(int argc, char **argv)
     ros::NodeHandle nh;
     ros::Subscriber map_sub = nh.subscribe("/map", 10, &mapCallback);
 
+    tf::TransformListener listener;
+    tf::StampedTransform robotPose;
+    while(ros::ok()) {
+        try {
+            listener.lookupTransform("map", "base_link", ros::Time(0), robotPose);
+            ROS_INFO_STREAM("robot pose! " << robotPose.getOrigin().x() << ", " << robotPose.getOrigin().y() << "\n");
+        } catch(tf::TransformException ex) {
+            ROS_ERROR("%s",ex.what());
+            ros::Duration(1.0).sleep();
+            continue;
+        }
+    }
     ros::spin();
     return 0;
 }
