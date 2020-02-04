557252e584fa270afc6d104b8372703e1e3e4885
Yuxiang Dai <yuxiang.dai43@gmail.com>
2020-02-04 17:43:04 -0500

    New motor code, now works in real bot

--- mie443_contest1/src/Movement.cpp
+++ mie443_contest1/src/Movement.cpp
@@ -5,6 +5,7 @@ class Move{
     private:
         ros::Publisher vel_pub;
         geometry_msgs::Twist vel;
+        double next_update;
 
     public:
     Move(ros::NodeHandle nh){
@@ -13,25 +14,23 @@ class Move{
     }
 
     void forward(float distance, float speed){
-        float next_update = ros::Time::now().toSec() + std::abs(distance) / std::abs(speed);
-        
+        next_update = ros::WallTime::now().toSec() + std::abs(distance) / std::abs(speed);
         vel.linear.x = speed * std::abs(distance) / distance;
         vel.angular.z = 0;
 
         ROS_INFO("Moving %f m, will take %f s, will finish at %f s", distance, std::abs(distance) / std::abs(speed), next_update);
-        while(ros::ok() && ros::Time::now().toSec() < next_update){
+        while(ros::ok() && ros::WallTime::now().toSec() < next_update){
             vel_pub.publish(vel);
         }
     }
 
     void rotate(float angle, float speed){
-        float next_update = ros::Time::now().toSec() + std::abs(angle) / std::abs(speed);
-        
+        next_update = ros::WallTime::now().toSec() + std::abs(angle) / std::abs(speed);
         vel.linear.x = 0;
         vel.angular.z = speed * std::abs(angle) / angle;
 
         ROS_INFO("Rotating %f, will take %f s, will finish at %f s", angle, std::abs(angle) / std::abs(speed), next_update);    
-        while(ros::ok() && ros::Time::now().toSec() < next_update){
+        while(ros::ok() && ros::WallTime::now().toSec() < next_update){
             vel_pub.publish(vel);
         }
     }
@@ -42,6 +41,10 @@ class Move{
         vel.angular.z = 0;
 
         vel_pub.publish(vel);
+    }
+
+    double get_next_update(){
+        return next_update;
     }
 };
 
--- mie443_contest1/src/contest1.cpp
+++ mie443_contest1/src/contest1.cpp
@@ -7,7 +7,7 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "image_listener");
     ros::NodeHandle nh;
- 
+
     Move move(nh);
     while (ros::ok())
     {
@@ -15,7 +15,7 @@ int main(int argc, char **argv)
 
         /// Logic goes here
         move.rotate(3.14/2, 1);
-        move.forward(-1, 0.5);
+        move.forward(0.25, 0.1);
         move.stop();
     }
 
