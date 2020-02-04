6a677445d5c8e3e6b91d8bd8654440ce8f2db39b
Nam <lkunelk@gmail.com>
2020-02-04 17:32:27 -0500

    publish world coordinates in world frame specified by gmapping

--- mie443_contest1/src/world_position.cpp
+++ mie443_contest1/src/world_position.cpp
@@ -5,6 +5,7 @@
 #include <ros/console.h>
 #include "ros/ros.h"
 #include <tf/transform_listener.h>
+#include <geometry_msgs/Point.h>
 
 int main(int argc, char **argv)
 {
@@ -12,11 +13,17 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "world_position");
     ros::NodeHandle nh;
 
+    ros::Publisher pub = nh.advertise<geometry_msgs::Point>("world_xy", 1000);
+
     tf::TransformListener listener;
     tf::StampedTransform robotPose;
     while(ros::ok()) {
         try {
             listener.lookupTransform("map", "base_link", ros::Time(0), robotPose);
+            geometry_msgs::Point msg;
+            msg.x = robotPose.getOrigin().x();
+            msg.y = robotPose.getOrigin().y();
+            pub.publish(msg);
             ROS_INFO_STREAM("robot pose! " << robotPose.getOrigin().x() << ", " << robotPose.getOrigin().y() << "\n");
         } catch(tf::TransformException ex) {
             ROS_ERROR("%s",ex.what());
