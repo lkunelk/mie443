29133ab350bea3bab1e27e1016c696788c093acc
Yuxiang Dai <yuxiang.dai43@gmail.com>
2020-02-04 16:20:17 -0500

    Code for low-level motion complete
    -Just import the Movement.cpp and just use the Move object
    -Example of usage can be found in Contest1.cpp

---
+++ mie443_contest1/src/Movement.cpp
@@ -0,0 +1,47 @@
+#include "ros/ros.h"
+#include <geometry_msgs/Twist.h>
+
+class Move{
+    private:
+        ros::Publisher vel_pub;
+        geometry_msgs::Twist vel;
+
+    public:
+    Move(ros::NodeHandle nh){
+        vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop", 1);
+        ROS_INFO("Mover initiated.");
+    }
+
+    void forward(float distance, float speed){
+        float next_update = ros::Time::now().toSec() + std::abs(distance) / std::abs(speed);
+        
+        vel.linear.x = speed * std::abs(distance) / distance;
+        vel.angular.z = 0;
+
+        ROS_INFO("Moving %f m, will take %f s, will finish at %f s", distance, std::abs(distance) / std::abs(speed), next_update);
+        while(ros::ok() && ros::Time::now().toSec() < next_update){
+            vel_pub.publish(vel);
+        }
+    }
+
+    void rotate(float angle, float speed){
+        float next_update = ros::Time::now().toSec() + std::abs(angle) / std::abs(speed);
+        
+        vel.linear.x = 0;
+        vel.angular.z = speed * std::abs(angle) / angle;
+
+        ROS_INFO("Rotating %f, will take %f s, will finish at %f s", angle, std::abs(angle) / std::abs(speed), next_update);    
+        while(ros::ok() && ros::Time::now().toSec() < next_update){
+            vel_pub.publish(vel);
+        }
+    }
+
+    void stop(){
+        ROS_INFO("STOPPED");
+        vel.linear.x = 0;
+        vel.angular.z = 0;
+
+        vel_pub.publish(vel);
+    }
+};
+
--- mie443_contest1/src/contest1.cpp
+++ mie443_contest1/src/contest1.cpp
@@ -1,135 +1,22 @@
-#include <ros/console.h>
+// THIS CODE IS NOT CONTEST CODE. It's just shows how to use Movement.h
+#include "Movement.cpp"
 #include "ros/ros.h"
-#include <geometry_msgs/Twist.h>
-#include <kobuki_msgs/BumperEvent.h>
-#include <sensor_msgs/LaserScan.h>
-#include <nav_msgs/Odometry.h>
-#include <tf/transform_datatypes.h>
-#include <stdio.h>
-#include <cmath>
 
-#include <chrono>
-
-#define N_BUMPER (3)
-#define RAD2DEG(rad) ((rad)*180. / M_PI)
-#define DEG2RAD(deg) ((deg)*M_PI / 180.)
-
-ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop", 1);
-geometry_msgs::Twist vel;
-
-uint8_t bumper[3] = {kobuki_msgs::BumperEvent::RELEASED, kobuki_msgs::BumperEvent::RELEASED, kobuki_msgs::BumperEvent::RELEASED};
-float minLaserDist = std::numeric_limits<float>::infinity();
-int32_t nLasers = 0, desiredNLasers = 0, desiredAngle = 5;
-
-float posX = 0.0, posY = 0.0, yaw = 0.0;
-
-void odomCallback(const nav_msgs::Odometry::ConstPtr &msg)
-{
-    posX = msg->pose.pose.position.x;
-    posY = msg->pose.pose.position.y;
-    yaw = tf::getYaw(msg->pose.pose.orientation);
-    //ROS_INFO("Position: (%f,%f) Orientation: %f rad or %f degrees.", posX, posY, yaw, RAD2DEG(yaw));
-}
-
-void bumperCallback(const kobuki_msgs::BumperEvent::ConstPtr &msg)
-{
-    //
-    // Access using bumper[kobuki_msgs::BumperEvent::{}] LEFT, CENTER, or RIGHT
-    bumper[msg->bumper] = msg->state;
-}
-
-void laserCallback(const sensor_msgs::LaserScan::ConstPtr &msg)
-{
-    minLaserDist = std::numeric_limits<float>::infinity();
-    nLasers = (msg->angle_max - msg->angle_min) / msg->angle_increment;
-    desiredNLasers = DEG2RAD(desiredAngle) / msg->angle_increment;
-    // ROS_INFO("Size of laser scan array: % i and size of offset: % i ", nLasers, desiredNLasers);
-
-    if (desiredAngle * M_PI / 180 < msg->angle_max && -desiredAngle * M_PI / 180 > msg->angle_min)
-    {
-        for (uint32_t laser_idx = nLasers / 2 - desiredNLasers; laser_idx < nLasers / 2 + desiredNLasers; ++laser_idx)
-        {
-            minLaserDist = std::min(minLaserDist, msg->ranges[laser_idx]);
-        }
-    }
-    else
-    {
-        for (uint32_t laser_idx = 0; laser_idx < nLasers; ++laser_idx)
-        {
-            minLaserDist = std::min(minLaserDist, msg->ranges[laser_idx]);
-        }
-    }
-}
-
-
-
-
-
-void forward(float distance, float speed){
-    float next_update = ros::Time::now().toSec() + std::abs(distance) / std::abs(speed);
-    
-    vel.linear.x = speed * std::abs(distance) / distance;
-    vel.angular.z = 0;
-
-    ROS_INFO("Moving %f m, will take %f s, will finish at %f s", distance, std::abs(distance) / std::abs(speed), next_update);
-
-    while(ros::ok() && ros::Time::now().toSec() < next_update){
-        vel_pub.publish(vel);
-    }
-}
-
-void rotate(float angle, float speed){
-    float next_update = ros::Time::now().toSec() + std::abs(angle) / std::abs(speed);
-    
-    vel.linear.x = 0;
-    vel.angular.z = speed * std::abs(angle) / angle;
-
-    ROS_INFO("Rotating %f, will take %f s, will finish at %f s", angle, std::abs(angle) / std::abs(speed), next_update);    
-    while(ros::ok() && ros::Time::now().toSec() < next_update){
-        vel_pub.publish(vel);
-    }
-}
-
-void stop(){
-    ROS_INFO("STOPPED");
-    vel.linear.x = 0;
-    vel.angular.z = 0;
-
-    vel_pub.publish(vel);
-}
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "image_listener");
     ros::NodeHandle nh;
-
-    ros::Subscriber bumper_sub = nh.subscribe("mobile_base/events/bumper", 10, &bumperCallback);
-    ros::Subscriber laser_sub = nh.subscribe("scan", 10, &laserCallback);
-    ros::Subscriber odom = nh.subscribe("odom", 1, &odomCallback);
-
-    ros::Rate loop_rate(10);
-
-
-    // contest count down timer
-    std::chrono::time_point<std::chrono::system_clock> start;
-    start = std::chrono::system_clock::now();
-    uint64_t secondsElapsed = 0;
-
  
-
-    while (ros::ok() && secondsElapsed <= 480)
+    Move move(nh);
+    while (ros::ok())
     {
         ros::spinOnce();
 
         /// Logic goes here
-
-        rotate(3.14/2, 1);
-        forward(1, 0.5);
-        stop(vel_pub, vel);
-
-        // The last thing to do is to update the timer.
-        secondsElapsed = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now() - start).count();
-        loop_rate.sleep();
+        move.rotate(3.14/2, 1);
+        move.forward(-1, 0.5);
+        move.stop();
     }
 
     return 0;
