20c76700af4ed931ddf472ac317b850246f53847
Nam <lkunelk@gmail.com>
2020-02-05 18:37:30 -0500

    print messages to screen

--- mie443_contest1/src/grid_position.cpp
+++ mie443_contest1/src/grid_position.cpp
@@ -9,10 +9,22 @@
 #include <tf/transform_listener.h>
 #include <geometry_msgs/Point.h>
 
+int origin_x, origin_y;
+int robot_x, robot_y;
+
 void mapCallback(const nav_msgs::OccupancyGrid::ConstPtr& msg)
 {
     geometry_msgs::Point p = msg->info.origin.position;
-    ROS_INFO_STREAM("map! " << p.x << ", " << p.y << ", " << p.z << "\n");
+    origin_x = p.x;
+    origin_y = p.y;
+    ROS_INFO_STREAM("map_origin");
+}
+
+void worldPositionCallback(const geometry_msgs::Point::ConstPtr& msg)
+{
+    robot_x = msg->x;
+    robot_y = msg->y;
+    ROS_INFO_STREAM("world_pose");
 }
 
 int main(int argc, char **argv)
@@ -21,6 +33,10 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "nam_node");
     ros::NodeHandle nh;
     ros::Subscriber map_sub = nh.subscribe("/map", 10, &mapCallback);
-    ros::spin();
+    ros::Subscriber world_position_sub = nh.subscribe("world_xy", 10, &worldPositionCallback);
+    while(ros::ok()) {
+        ROS_INFO_STREAM("world: " << origin_x << ", " << origin_y << " robot: " << robot_x << ", " << robot_y << "\n");
+        ros::spinOnce();
+    }
     return 0;
 }
