06da12410f87ac8a615a98d3448712b4dbaa6c2c
yuxiangdai <yuxiang.dai43@gmail.com>
2020-02-05 17:15:50 -0500

    proper BFS

--- mie443_contest1/src/arrays.cpp
+++ mie443_contest1/src/arrays.cpp
@@ -2,8 +2,6 @@
 // a given source cell to a destination cell. 
 #include <bits/stdc++.h> 
 using namespace std; 
-#define ROW 9 
-#define COL 10 
 
 //To store matrix cell cordinates 
 struct Point 
@@ -21,10 +19,17 @@ struct queueNode
 
 // check whether given cell (row, col) is a valid 
 // cell or not. 
-bool isValid(int row, int col) 
+bool isValid(int row, int col, int ROW, int COL) 
 { 
 	// return true if row number and column number 
 	// is in range 
+    if(row == 2 && col == 3) {
+        cout << "row 2 col 3: ";
+        cout << (row >= 0) && (row < ROW) && 
+		    (col >= 0) && (col < COL);
+        cout << endl;
+    }
+         
 	return (row >= 0) && (row < ROW) && 
 		(col >= 0) && (col < COL); 
 } 
@@ -36,27 +41,32 @@ int colNum[] = {0, -1, 1, 0};
 
 void printpath(vector<queueNode>& path) 
 { 
-    cout << "print path";
+    cout << "Path: ";
     int size = path.size(); 
     for (int i = 0; i < size; i++)  
         cout << path[i].pt.x << "," << path[i].pt.y << " ";     
     cout << endl; 
 } 
 
+int convert2Dto1D(int row, int col, int numCol)
+{
+    return col + numCol * row;
+}
+
 // function to find the shortest path between 
 // a given source cell to a destination cell. 
-int BFS(int mat[][COL], Point src, Point dest) 
+int BFS(int mat[], int numRow, int numCol, Point src) 
 { 
 	// check source and destination cell 
 	// of the matrix have value 1 
-	if (!mat[src.x][src.y] || !mat[dest.x][dest.y]) 
+	if (!mat[convert2Dto1D(src.x, src.y, numCol)]) 
 		return -1; 
 
-	bool visited[ROW][COL]; 
+	bool visited[convert2Dto1D(numRow, numCol, numCol) - 1]; 
 	memset(visited, false, sizeof visited); 
 	
 	// Mark the source cell as visited 
-	visited[src.x][src.y] = true; 
+	visited[convert2Dto1D(src.x, src.y, numCol)] = true; 
 
 	// Create a queue for BFS 
 	queue<vector<queueNode> > q; 
@@ -72,8 +82,8 @@ int BFS(int mat[][COL], Point src, Point dest)
 	// Do a BFS starting from source cell 
 	while (!q.empty()) 
 	{ 
-
         path = q.front();
+        // printpath(path);
 
 		queueNode curr = path[path.size() - 1]; 
 		Point pt = curr.pt; 
@@ -82,7 +92,9 @@ int BFS(int mat[][COL], Point src, Point dest)
 		// If we have reached the destination cell, 
 		// we are done 
 		// if (mat[pt.x][pt.y] == 20 || pt.x == dest.x && pt.y == dest.y) 
-		if (mat[pt.x][pt.y] == 20) 
+		
+        // cout << convert2Dto1D(pt.x, pt.y, numCol) << " ";
+        if (mat[convert2Dto1D(pt.x, pt.y, numCol)] == 4) 
         {
             // int test = curr.dist;
             printpath(path);
@@ -100,17 +112,20 @@ int BFS(int mat[][COL], Point src, Point dest)
 			int row = pt.x + rowNum[i]; 
 			int col = pt.y + colNum[i]; 
 
-			
+			cout << row << "," << col << "," << convert2Dto1D(row, col, numCol);
+			cout << "valid?:" << isValid(row, col, numRow, numCol);
+            cout << endl; 
+
 			// if adjacent cell is valid, has path and 
 			// not visited yet, enqueue it. 
-			if (isValid(row, col) && mat[row][col] && 
-			!visited[row][col]) 
+			if (isValid(row, col, numRow, numCol) && mat[convert2Dto1D(row, col, numCol)] && 
+			!visited[convert2Dto1D(row, col, numCol)]) 
 			{ 
 				// mark cell as visited and enqueue it 
-				visited[row][col] = true; 
+                // cout << row << "," << col << "," << convert2Dto1D(row, col, numCol);
+				visited[convert2Dto1D(row, col, numCol)] = true; 
 				queueNode Adjcell = { {row, col}, 
 									curr.dist + 1 }; 
-
 
 
                 vector<queueNode> newpath(path);
@@ -129,23 +144,20 @@ int BFS(int mat[][COL], Point src, Point dest)
 // Driver program to test above function 
 int main() 
 { 
-	int mat[ROW][COL] = 
-	{ 
-		{ 1, 0, 1, 1, 1, 1, 0, 1, 1, 1 }, 
-		{ 1, 0, 1, 0, 1, 1, 1, 0, 1, 1 }, 
-		{ 1, 1, 1, 0, 1, 1, 0, 1, 0, 1 }, 
-		{ 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 }, 
-		{ 1, 1, 1, 0, 1, 1, 1, 0, 1, 0 }, 
-		{ 1, 0, 1, 1, 20, 1, 0, 1, 0, 0 }, 
-		{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 }, 
-		{ 1, 0, 1, 1, 1, 1, 0, 1, 1, 1 }, 
-		{ 1, 1, 0, 0, 0, 0, 1, 0, 0, 1 } 
+    int ROW = 5;
+    int COL = 5;
+	int mat[convert2Dto1D(ROW, COL, COL) - 1] = 
+	{  
+		  1, 0, 1, 1, 1,
+          1, 1, 1, 0, 1,
+          0, 0, 0, 0, 1,
+          1, 1, 1, 1, 1,
+          1, 1, 1, 4, 0
 	}; 
 
 	Point source = {0, 0}; 
-	Point dest = {3, 4}; 
 
-	int dist = BFS(mat, source, dest); 
+	int dist = BFS(mat, ROW, COL, source); 
 
 	if (dist != INT_MAX) 
 		cout << "Shortest Path is " << dist ; 
