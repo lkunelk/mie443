11c0b8d98a705a371b6aae3d532889f86ed07549
Nam <lkunelk@gmail.com>
2020-02-06 10:56:57 -0500

    publish grid position

--- mie443_contest1/src/grid_position.cpp
+++ mie443_contest1/src/grid_position.cpp
@@ -31,6 +31,7 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "nam_node");
     ros::NodeHandle nh;
     ros::Subscriber map_sub = nh.subscribe("/map", 10, &mapCallback);
+    ros::Publisher pub = nh.advertise<geometry_msgs::Point>("world_xy", 1000);
 
     tf::TransformListener listener;
     tf::StampedTransform robotPose;
@@ -38,11 +39,23 @@ int main(int argc, char **argv)
         try {
             listener.lookupTransform("map", "base_link", ros::Time(0), robotPose);
             ROS_INFO_STREAM("robot pose! " << robotPose.getOrigin().x() << ", " << robotPose.getOrigin().y() << "\n");
+
+            float origin_to_robot_x = robotPose.getOrigin().x() - origin_x;
+            float origin_to_robot_y = robotPose.getOrigin().y() - origin_y;
+            float grid_x = origin_to_robot_x / resolution;
+            float grid_y = origin_to_robot_y / resolution;
+
+            geometry_msgs::Point msg;
+            msg.x = grid_x;
+            msg.y = grid_y;
+            pub.publish(msg);
+
         } catch(tf::TransformException ex) {
             ROS_ERROR("%s",ex.what());
             ros::Duration(1.0).sleep();
             continue;
         }
+        ros::spinOnce();
     }
 
     return 0;
