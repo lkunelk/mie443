a71a0580d813b6d0cfe8f4216be353d18eafe59f
TheHong <chcheung.office@gmail.com>
2020-02-25 18:37:22 -0500

    Launch file beginnings

---
+++ mie443_contest2/src/test.cpp
@@ -0,0 +1,13 @@
+#include <boxes.h>
+#include <navigation.h>
+#include <robot_pose.h>
+#include <imagePipeline.h>
+
+int main(int argc, char** argv) {
+    // Setup ROS.
+    ros::init(argc, argv, "test");
+    ros::NodeHandle n;
+
+    std::cout<<"Hello";
+    return 0;
+}
